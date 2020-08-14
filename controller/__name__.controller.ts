07d62c352e1c86353d419a6d026bdc9b3c63517d
SnakeN <9453737@qq.com>
2020-08-14 17:41:02 +0800

    update controller file

--- controller/__name__.controller.ts
+++ controller/__name__.controller.ts
@@ -1,6 +1,7 @@
 import { Controller } from "@nestjs/common";
 
 import { __Name__Service } from "../service";
+
 @Controller("__name__")
 export class __Name__Controller {
   constructor(private readonly __name__Service: __Name__Service) {}
