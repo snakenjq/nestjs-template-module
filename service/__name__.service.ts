854aae4f12e7b002449058cb469d7e9a17b47d1b
SnakeN <9453737@qq.com>
2020-08-16 01:37:59 +0800

    update service file

--- service/__name__.service.ts
+++ service/__name__.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
+import { Repository } from "typeorm";
 
+import { __Name__ } from "../model";
 @Injectable()
-export class __Name__Service {}
+export class __Name__Service {
+  constructor(
+    @InjectRepository(__Name__)
+    private readonly __name__Repository: Repository<__Name__>
+  ) {}
+}
